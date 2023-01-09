00ab09b9e3ca88bcef81588d4705b93b4c6944c4
Dylan Landry <dylan.landry@milkmoovement.com>
2023-01-09 07:36:35 -0500

    add mason, lspconfig, nvim-cmp, telescope, nvim-treesitter, and more

--- init.lua
+++ init.lua
@@ -1,13 +1,205 @@
-vim.keymap.set('i', 'jk', '<Esc>', { desc = 'exit insert mode'})
-
+vim.keymap.set('i', 'jk', '<Esc>', { desc = 'exit insert mode' })
 vim.opt.number = true
 vim.opt.relativenumber = true
-
 vim.opt.scrolloff = 4
-
 vim.opt.wrap = false
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.signcolumn = 'yes'
+vim.opt.updatetime = 100
+vim.g.mapleader = ' '
 
-vim.opt.tabstop = 2          -- Best descriptions of tabstop, softabstop, and shiftwidth I've found:
-vim.opt.softtabstop = 2      -- https://arisweedler.medium.com/tab-settings-in-vim-1ea0863c5990#:~:text=tabstop%20is%20effectively%20how%20many,a%20backspace%20keypress%20is%20worth.
-vim.opt.shiftwidth = 2       -- https://www.reddit.com/r/vim/comments/99ylz8/confused_about_the_difference_between_tabstop_and/
+-- Plugins
+require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+	use 'neovim/nvim-lspconfig'
+	use 'hrsh7th/nvim-cmp'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/cmp-nvim-lsp-signature-help'
+	use({ "L3MON4D3/LuaSnip", tag = "v1.*" })
+	use 'navarasu/onedark.nvim'
+	use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
+	use 'j-hui/fidget.nvim'
+	use 'tpope/vim-surround'
+	use 'tpope/vim-repeat'
+	use 'tpope/vim-fugitive'
+	use 'tpope/vim-sleuth'
+	use 'airblade/vim-gitgutter'
+	use { 'nvim-telescope/telescope.nvim', tag = '0.1.0', requires = { 'nvim-lua/plenary.nvim' } }
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+	use 'williamboman/mason.nvim'
+	use 'williamboman/mason-lspconfig.nvim'
+	use 'numToStr/Comment.nvim'
+end)
+require "fidget".setup()
+require('onedark').load()
+require('Comment').setup()
 
+-- Setup completion plugin
+local cmp = require('cmp')
+cmp.setup({
+	mapping = cmp.mapping.preset.insert({
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp', group_index = 1 },
+		{ name = 'luasnip', group_index = 1 },
+		{ name = 'buffer', group_index = 2 },
+		{ name = 'nvim_lsp_signature_help' }
+	}),
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
+})
+local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- Setup lsp configs
+local on_buffer_attach_to_lsp_client = function(_, buffer_number)
+	local nmap = function(keys, func, desc)
+		vim.keymap.set('n', keys, func, { buffer = buffer_number, desc = desc })
+	end
+	nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+	nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+	nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+	nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+	nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+	nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+	nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+	nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+	nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+	nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+	nmap(']d', vim.diagnostic.goto_next, 'Go to next [d]iagnostic')
+	nmap('[d', vim.diagnostic.goto_prev, 'Go to prev [d]iagnostic')
+	vim.api.nvim_buf_create_user_command(buffer_number, 'Format', function()
+		local eslint_client = vim.lsp.get_active_clients({ bufnr = buffer_number, name = 'eslint' })[1]
+		if eslint_client then
+			-- no clue why this is causing a treesitter highlight problem
+			vim.cmd('EslintFixAll')
+		else
+			vim.lsp.buf.format()
+		end
+	end, { desc = 'Format current buffer with LSP' })
+	local format_on_save_group = vim.api.nvim_create_augroup('Format on save', { clear = true })
+	vim.api.nvim_create_autocmd('BufWritePre', { buffer = buffer_number, group = format_on_save_group, command = 'Format' })
+end
+local server_settings = {
+	sumneko_lua = {
+		Lua = {
+			diagnostics = {
+				globals = { 'vim' }
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
+			},
+		}
+	},
+	rust_analyzer = {
+		imports = {
+			granularity = {
+				group = "module",
+			},
+			prefix = "self",
+		},
+		cargo = {
+			buildScripts = {
+				enable = true,
+			},
+		},
+		procMacro = {
+			enable = true
+		},
+	}
+}
+require('mason').setup()
+local mason_lspconfig = require('mason-lspconfig')
+mason_lspconfig.setup_handlers {
+	function(server_name)
+		require('lspconfig')[server_name].setup {
+			capabilities = cmp_capabilities,
+			on_attach = on_buffer_attach_to_lsp_client,
+			settings = server_settings[server_name],
+		}
+	end,
+}
+
+require('lualine').setup {
+	options = {
+		theme = 'auto',
+		component_separators = { left = '', right = '' },
+		section_separators = { left = '', right = '' },
+		disabled_filetypes = {
+			statusline = {},
+			winbar = {},
+		},
+		ignore_focus = {},
+		always_divide_middle = true,
+		globalstatus = false,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+		}
+	},
+	sections = {
+		lualine_a = { 'mode' },
+		lualine_b = { 'branch', 'diff', 'diagnostics' },
+		lualine_c = { 'filename' },
+		lualine_x = { 'encoding', 'fileformat', 'filetype' },
+		lualine_y = { 'progress' },
+		lualine_z = { 'location' }
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { 'filename' },
+		lualine_x = { 'location' },
+		lualine_y = {},
+		lualine_z = {}
+	},
+	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = {}
+}
+
+require('nvim-treesitter.configs').setup({
+	auto_install = true,
+	highlight = {
+		enable = true,
+	},
+})
+
+local telescope = require('telescope')
+telescope.setup {
+	defaults = {
+		mappings = {
+			i = {
+				['<C-u>'] = false,
+				['<C-d>'] = false,
+			},
+		},
+	},
+}
+pcall(telescope.load_extension, 'fzf')
+local telescope_builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>sk', telescope_builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+vim.keymap.set('n', '<leader>sh', telescope_builtin.help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sf', telescope_builtin.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sg', telescope_builtin.live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sw', telescope_builtin.grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>?', telescope_builtin.oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', telescope_builtin.buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>/', telescope_builtin.current_buffer_fuzzy_find,
+	{ desc = '[/] Fuzzily search in current buffer]' })
+vim.keymap.set('n', '<leader>sd', telescope_builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
