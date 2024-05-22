8af6bdffa1d436f495a0b187ad6add52c8e0eb40
dylan <dylanglandry@gmail.com>
2024-05-21 21:00:39 -0400

    add autocmd for md file options

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 vim.keymap.set('i', 'jk', '<Esc>')
 vim.opt.relativenumber = true
 vim.opt.number = true
+
+vim.api.nvim_create_autocmd('BufEnter', {
+	pattern = '*.md',
+	callback = function()
+		vim.opt.wrap = true
+		vim.opt.linebreak = true
+	end
+})
