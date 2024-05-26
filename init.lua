8bd6d8c07f68229e73f95f73c8a1a54a61ed3a6e
Dylan Landry <dylan@tactustherapy.com>
2024-05-26 11:32:57 -0400

    add lazy plugin manager

--- init.lua
+++ init.lua
@@ -3,9 +3,26 @@ vim.opt.relativenumber = true
 vim.opt.number = true
 
 vim.api.nvim_create_autocmd('BufEnter', {
-	pattern = '*.md',
-	callback = function()
-		vim.opt.wrap = true
-		vim.opt.linebreak = true
-	end
+    pattern = '*.md',
+    callback = function()
+        vim.opt.wrap = true
+        vim.opt.linebreak = true
+    end
 })
+
+-- Bootstrap plugin manager called "lazy.nvim"
+-- If lazy is not in user's data directory, git clone it.
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+-- Add lazy to runtimepath
+vim.opt.rtp:prepend(lazypath)
+
