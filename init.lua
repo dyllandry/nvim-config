3b7f2b58ff4f16d949493b03490f452a7dea77b2
Dylan Landry <dylan@tactustherapy.com>
2024-05-28 11:49:31 -0400

    add spacing between plugins

--- init.lua
+++ init.lua
@@ -31,11 +31,13 @@ require("lazy").setup({
     -- Automatically set tab settings.
     -- Will set according to project's editorconfig file.
     'tpope/vim-sleuth',
+
     -- Comment plugin.
     -- Eventually want to add treesitter and integrate with
     -- "JoosepAlviste/nvim-ts-context-commentstring" to support commenting
     -- embedded languages like in vue components.
     { 'numToStr/Comment.nvim', config = true, lazy = false },
+
     -- File tree explorer.
     {
       'preservim/nerdtree',
