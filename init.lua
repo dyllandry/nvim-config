0b92f77ab51c50783420c3d4ef311d09e8181bea
Dylan Landry <dylan@tactustherapy.com>
2024-06-10 11:52:20 -0400

    added lsp config
    Included comments from old nvim config. Will refine as time goes on.

--- init.lua
+++ init.lua
@@ -1,4 +1,12 @@
 --------------------------------------------------------------------------------
+-- Future Todo -----------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- Get prettier working somehow.
+--
+-- Use none-ls (was null-ls before that was deprecated) to support nvim.prettier
+-- and let other non-lsp sources interface with nvim's lsp client.
+
+--------------------------------------------------------------------------------
 -- Generic Options -------------------------------------------------------------
 --------------------------------------------------------------------------------
 
@@ -160,20 +168,263 @@ require("lazy").setup({
                 -- Enables highlighting based off the built syntax tree.
                 highlight = { enable = true },
                 -- Makes the = command indent the right number of tabs/spaces.
-                indent = { enable = true },
+                -- indent = { enable = true },
             })
         end
-        },
+    },
 
-        -- nvim-ts-context-commentstring is a plugin that sets 'commentstring'
-        -- depending on where the cursor is and uses treesitter. Seems
-        -- redundant with Comment.nvim, but Comment.nvim itself didn't
-        -- implement all the complicated logic for how to properly comment
-        -- embedded languages in typescript. However, this plugin,
-        -- nvim-ts-context-commentstring, did. I think the two plugins work
-        -- together. This plugin is already installed by setting it as a
-        -- dependency of other plugins, but put it here as as standalone plugin
-        -- to centralize this comment.
-        "JoosepAlviste/nvim-ts-context-commentstring"
+    -- nvim-ts-context-commentstring is a plugin that sets 'commentstring'
+    -- depending on where the cursor is and uses treesitter. Seems
+    -- redundant with Comment.nvim, but Comment.nvim itself didn't
+    -- implement all the complicated logic for how to properly comment
+    -- embedded languages in typescript. However, this plugin,
+    -- nvim-ts-context-commentstring, did. I think the two plugins work
+    -- together. This plugin is already installed by setting it as a
+    -- dependency of other plugins, but put it here as as standalone plugin
+    -- to centralize this comment.
+    "JoosepAlviste/nvim-ts-context-commentstring",
+
+    -- nvim-lspconfig: Nvim supports the Language Server Protocol (LSP). If you
+    -- have language servers available on your computer, then Nvim can connect
+    -- to them and provide cool features like jumping to definition, renaming,
+    -- etc. Nvim has to be configured for each language server, which can be
+    -- hard to configure it correctly, so this plugin nvim-lspconfig provides
+    -- some ready-to-go configs. You can checkout `:help lspconfig-all` to see
+    -- the list of configurations, and installation instructions to get the
+    -- respective language server on your computer.
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+        },
+        config = function()
+            local lspconfig = require('lspconfig')
+            -- nvim-cmp supports more LSP capabilities than omnifunc.
+            -- We will want to inform language servers of this.
+            local nvim_cmp_capabilities = require("cmp_nvim_lsp")
+                .default_capabilities()
+            -- Makes it so that if you have multiple LSPs reporting
+            -- diagnostics, the virtual text that appears on the buffer, and
+            -- the floating window that shows when you jump to a diagnostic,
+            -- both show the source of the diagnostic.
+            vim.diagnostic.config({
+                virtual_text = { source = true },
+                float = { source = 'always' }
+            })
+            lspconfig.tsserver.setup({
+                handlers = {
+                    ["textDocument/publishDiagnostics"] = function(
+                        _,
+                        result,
+                        ctx,
+                        config
+                    )
+                        if result.diagnostics ~= nil then
+                            local idx = 1
+                            while idx <= #result.diagnostics do
+                                if result.diagnostics[idx].code == 80001 then
+                                    table.remove(result.diagnostics, idx)
+                                else
+                                    idx = idx + 1
+                                end
+                            end
+                        end
+                        vim.lsp.diagnostic.on_publish_diagnostics(
+                            _,
+                            result,
+                            ctx,
+                            config
+                        )
+                    end,
+                },
+            })
+            -- Disabled 2024/07/06 to limit complexity while I'm getting things
+            -- working again. Next will probably just get TS working, then
+            -- follow instructions for vue support. I think instructions
+            -- changed since the last time this nvim config was written.
+            -- LSP for Vue 3
+            -- lspconfig.volar.setup {
+            --     -- Turns on 'Take Over Mode' so volar becomes the language
+            --     -- server for TypeScript files and can provide proper
+            --     -- support for .vue files imported into TypeScript files.
+            --     filetypes = {
+            --         'typescript',
+            --         'javascript',
+            --         'javascriptreact',
+            --         'typescriptreact',
+            --         'vue',
+            --         'json'
+            --     },
+            --     init_options = {
+            --         typescript = {
+            --             -- Some projects I use volar in that are js projects
+            --             -- don't have typescript as a dependency, so here I
+            --             -- point volar to use a globally installed typescript.
+            --             -- Should see if I can do node/**/bin in file path so
+            --             -- it can use any version. Or make a version that uses
+            --             -- the current node version from nvm in the file path.
+            --             tsdk = '/Users/dylan/.nvm/versions/node/v19.6.1/lib/node_modules/typescript/lib'
+            --         }
+            --     },
+            --     -- Hides tsserver's specific diagnostic hint to convert
+            --     -- commonjs modules to es6 modules.
+            --     -- https://www.reddit.com/r/neovim/comments/nv3qh8/comment/h1tx1rh/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+            --     handlers = {
+            --         ["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
+            --             if result.diagnostics ~= nil then
+            --                 local idx = 1
+            --                 while idx <= #result.diagnostics do
+            --                     if result.diagnostics[idx].code == 80001 then
+            --                         table.remove(result.diagnostics, idx)
+            --                     else
+            --                         idx = idx + 1
+            --                     end
+            --                 end
+            --             end
+            --             vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+            --         end,
+            --     },
+            --     capabilities = nvim_cmp_capabilities
+            -- }
+            -- Eslint is a linting engine for JS and TS. This LSP shows it's
+            -- issues as diagnostics, and provides a command for automatically
+            -- fixing issues.
+            -- Would be cool on js/jsx/ts/tsx files on save to automatically run eslint then prettier.
+            -- Disabled for now, want to turn it on incrementally to verify it
+            -- is working how I expected.
+            lspconfig.eslint.setup({
+                -- I don't know why this is commented out.
+                -- on_attach = function(client, bufnr)
+                --     vim.api.nvim_create_autocmd("BufWritePre", {
+                --         buffer = bufnr,
+                --         command = "EslintFixAll",
+                --     })
+                -- end,
+                capabilities = nvim_cmp_capabilities
+            })
+            -- Disabled for now to limit complexity.
+            -- Config for Markdown language server. Big reason I want it is
+            -- that in addition to regular markdown features like linking
+            -- between files `[link to yesterday's file](/yesterday.md)`,
+            -- marksman supports linking to specific headers in a file: `[link
+            -- to yesterday's file](/yesterday)`
+            -- lspconfig.marksman.setup {
+            --     capabilities = nvim_cmp_capabilities
+            -- }
+            -- Below we register keymaps after Nvim attaches to a language server.
+            -- This autocmd will run whenever the LspAttach event fires.
+            vim.api.nvim_create_autocmd('LspAttach', {
+                -- Puts this autocmd in a group. Useful for organizing
+                -- and using the group name as an identifier to later
+                -- remove them. :help autocmd-groups
+                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+                -- This is the event handler when the autocommand runs.
+                callback = function(ev)
+                    -- The LspAttach event includes which buffer was attached.
+                    -- We can use this buffer number when registering keymaps
+                    -- to make the keymap only work in that buffer.
+                    local options = { buffer = ev.buf }
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, options)
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, options)
+                    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, options)
+                    -- References are anywhere a symbol appears. Shows a list
+                    -- of symbol occurences informed by usage provided by the
+                    -- language server. Upon navigating, puts the cursor on the
+                    -- occurence.
+                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, options)
+                    -- The implementation is where a symbol is used. If you do
+                    -- it on a base class, shows a list of derived classes. If
+                    -- you do it on a type, shows a list of variables with that
+                    -- type. Upon navigating, puts the cursor on the symbol
+                    -- which implements the original symbol.
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, options)
+                    vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, options)
+                    vim.keymap.set(
+                        { 'n', 'v' },
+                        '<Leader>ca',
+                        vim.lsp.buf.code_action,
+                        options
+                    )
+                    vim.keymap.set({ 'n' }, ']d', vim.diagnostic.goto_next, options)
+                    vim.keymap.set({ 'n' }, '[d', vim.diagnostic.goto_prev, options)
+                    -- Disabled for now, not sure if this is my desired behaviour.
+                    -- vim.keymap.set(
+                    --     'n',
+                    --     '<Leader>f',
+                    --     function()
+                    --         vim.lsp.buf.format { async = true }
+                    --     end,
+                    --     opts
+                    -- )
+                    -- This was a TODO from previous nvim configuration.
+                    -- TODO: install, configure, test
+                    -- require("lsp_signature").on_attach({}, ev.buf)
+                end,
+            })
+        end
+    },
+    -- Autocompletion plugin. It's the fancy window that automatically appears and as
+    -- you type and fills with suggestions. A replacement of vim's omnifunc manual
+    -- completion tool.
+    -- For it to work "completion sources" must be installed and "sourced". nvim-cmp
+    -- will handle the rest.
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            -- LSP source for nvim-cmp. Lets nvim-cmp work with the language server's
+            -- completion results.
+            'hrsh7th/cmp-nvim-lsp',
+            -- nvim-cmp needs a snippet engine to work.
+            'L3MON4D3/LuaSnip',
+            -- This is the source for nvim-cmp to connect to luasnip
+            'saadparwaiz1/cmp_luasnip',
+        },
+        config = function()
+            local cmp = require('cmp')
+            local luasnip = require('luasnip')
+
+            cmp.setup {
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-k>'] = cmp.mapping.close(),
+                    ['<CR>'] = cmp.mapping.confirm {
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true,
+                    },
+                    ['<Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+                        else
+                fallback()
+                        end
+                    end, { 'i', 's' }),
+                    ['<S-Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+                        else
+                fallback()
+                        end
+                    end, { 'i', 's' }),
+                }),
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                },
+            }
+        end
+    },
+
 })
 
